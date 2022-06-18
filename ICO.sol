9340e0305d6cdbbe6e6a9f250cb66ad887dab41f
Starkk <42775517+prabal-007@users.noreply.github.com>
2022-06-18 15:13:12 +0530

    Update ICO.sol

--- ICO.sol
+++ ICO.sol
@@ -1,7 +1,8 @@
 // ICO contract
 //SPDX-License-Identifier: UNLICENSED
 
-pragma solidity >=0.5.0 <0.9.0; // ---------------------------------------------------------------------------- // EIP-20: ERC-20 Token Standard // https://eips.ethereum.org/EIPS/eip-20 // -----------------------------------------
+pragma solidity >=0.5.0 <0.9.0; 
+// EIP-20: ERC-20 Token Standard // https://eips.ethereum.org/EIPS/eip-20 // 
 
 interface ERC20Interface { function totalSupply() external view returns (uint); function balanceOf(address tokenOwner) external view returns (uint balance); function transfer(address to, uint tokens) external returns (bool success);
 
